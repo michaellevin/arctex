5048028b5959253c5897af16abd0ec0b333c1ea4
Artem Yagodin <ecce.artemis@gmail.com>
2023-11-07 12:44:40 -0800

    Map added

--- flt/lib/pages/assets_page.dart
+++ flt/lib/pages/assets_page.dart
@@ -1,5 +1,13 @@
 import 'package:arktech/widgets/tree_widget.dart';
+import 'package:syncfusion_flutter_maps/maps.dart';
 import 'package:flutter/material.dart';
+
+
+/// Collection of Australia state code data.
+class Model {
+  final String state;
+  const Model(this.state);
+}
 
 
 class AssetsPage extends StatefulWidget {
@@ -11,12 +19,32 @@ class AssetsPage extends StatefulWidget {
 
 
 class _AssetsPageState extends State<AssetsPage> {
+  late MapShapeSource _mapSource;
+  late List<Model> _data;
+
+  @override
+  void initState() {
+    _mapSource = MapShapeSource.asset(
+      'assets/australia.json',
+      shapeDataField: 'STATE_NAME',
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         TreeWidget(),
-        // Expanded(child: const Text("Assets")),
+        Expanded(
+          child: SfMaps(
+            layers: [
+              MapShapeLayer(
+                source: _mapSource
+              )
+            ]
+          )
+        ),
       ],
     );
   }

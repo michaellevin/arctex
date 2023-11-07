567d3d7ece97054f44140320ef33872019b215f1
Artem Yagodin (Hi Tech Talents LLC) <v-ayagodin@microsoft.com>
2023-11-07 09:27:44 -0800

    side menu added

---
+++ flt/lib/pages/assets_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class AssetsPage extends StatefulWidget {
+  const AssetsPage({super.key});
+
+  @override
+  State<AssetsPage> createState() => _AssetsPageState();
+}
+
+
+class _AssetsPageState extends State<AssetsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Assets");
+  }
+}

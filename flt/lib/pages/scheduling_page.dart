a9f0dc70ef9b7ffb9eff6e67e781dacdff7aaafa
Michael Levin <michael.levi.fx@gmail.com>
2023-11-18 23:14:18 +0200

    Refactor code for improved readability and performance

---
+++ flt/lib/pages/scheduling_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class SchedulingPage extends StatefulWidget {
+  const SchedulingPage({super.key});
+
+  @override
+  State<SchedulingPage> createState() => _SchedulingPageState();
+}
+
+
+class _SchedulingPageState extends State<SchedulingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Scheduling");
+  }
+}
---
+++ pyt/core/adapters/__init__.py
@@ -0,0 +1,2 @@
+from .base_adapter import BaseAdapter
+from .strategy import CSVStrategy

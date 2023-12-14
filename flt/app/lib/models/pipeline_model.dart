2fcc2f6722c95d64e2e3f44942472b7f4aa777e5
Artem Yagodin <ecce.artemis@gmail.com>
2023-12-13 18:22:03 -0800

    form rows detailes added

--- flt/app/lib/models/pipeline_model.dart
+++ flt/app/lib/models/pipeline_model.dart
@@ -1,12 +1,30 @@
 import 'package:arktech/models/itree_node_model.dart';
 
 class PipelineModel extends AbsPipelineModel {
+  bool pigging, cathodicProtection, externalCoating;
+  String cpType, environment, pipelineDesignCode;
+  double designCorrosionRate, designTemperature, designPressure, designFlow;
+  DateTime startDate;
+
   PipelineModel({
     required String id,
     String? parentId,
     required String name,
     required TreeNodeType type,
+
+    required this.environment,
+    required this.pigging,
+    required this.cathodicProtection,
+    required this.externalCoating,
+    required this.cpType,
+    required this.pipelineDesignCode,
+    required this.designCorrosionRate,
+    required this.designTemperature,
+    required this.designPressure,
+    required this.designFlow,
+    required this.startDate,    
   })  : super(id: id, parentId: parentId, name: name, type: type);
+        
 
   static PipelineModel fromJson(Map<String, dynamic> json) {
     return PipelineModel(
@@ -14,6 +32,18 @@ class PipelineModel extends AbsPipelineModel {
       parentId: json['parentId'],
       name: json['name'],
       type: TreeNodeType.pipeline,
+
+      environment: json['environment'] ?? '',
+      pigging: json['pigging'] ?? false,
+      cathodicProtection: json['cathodicProtection'] ?? false,
+      externalCoating: json['externalCoating'] ?? false,
+      cpType: json['cpType'] ?? '',
+      pipelineDesignCode: json['pipelineDesignCode'] ?? '',
+      designCorrosionRate: json['designCorrosionRate'] ?? 0.0,
+      designTemperature: json['designTemperature'] ?? 0.0,
+      designPressure: json['designPressure'] ?? 0.0,
+      designFlow: json['designFlow'] ?? 0.0,
+      startDate: json['startDate'] != null ? DateTime.parse(json['startDate']) : DateTime.utc(1969),
     );
   }
 }
--- flt/app/lib/widgets/pp_parameter_widget.dart
+++ flt/app/lib/widgets/pp_parameter_widget.dart
@@ -15,19 +15,24 @@ enum PPParamType {
 class PPParamWidget extends StatelessWidget {
   final PPParamType type;
   final String label;
-  final String value;
+  final dynamic value;
 
-  const PPParamWidget({
+  late final TextEditingController _controller;
+
+  PPParamWidget({
     required this.type, 
     required this.label,
     required this.value,
     super.key
-  });
+  }) {
+    _controller = TextEditingController(text: value.toString());
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(3.0),
+      padding: const EdgeInsets.all(6.0),
       child: Row(
         children: [
           SizedBox(
@@ -37,17 +42,30 @@ class PPParamWidget extends StatelessWidget {
               style: const TextStyle(fontWeight: FontWeight.bold),
             )
           ),
+          const SizedBox(width: 10),
           SizedBox(
-            width: 500,
+            width: 400,
             height: 50,
             child: TextField(
+              controller: _controller,
               enabled: false,
               expands: true,
               maxLines: null,
               minLines: null,
               decoration: InputDecoration(
+                contentPadding: EdgeInsets.only(left: 10.0),
                 border: const OutlineInputBorder(),
-                labelText: value,
+                suffixText: "kg/m3  ",
+                // suffixIcon: const Align(
+                //   alignment: Alignment.centerRight,
+                //   child: Padding(
+                //     padding: EdgeInsets.all(1.0),
+                //     child: Text(
+                //       "kg/m3",
+                //       // style: const TextStyle(fontWeight: FontWeight.bold),
+                //     ),
+                //   ),
+                // ),
               ),
             ),
           )

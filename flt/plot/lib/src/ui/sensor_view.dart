f7356b893c00fc85a2bf121378a630382f5ef2d6
Michael Levin <michael.levi.fx@gmail.com>
2024-01-06 02:45:41 +0200

    feat: add saving new sensor to JSON

--- flt/plot/lib/src/ui/sensor_view.dart
+++ flt/plot/lib/src/ui/sensor_view.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 
 import 'sensor_item.dart';
+import 'add_sensor_item_alert.dart';
 
 import '../models/sensors/sensor_abstract.dart'; // Import Sensor model
 import '../models/sensors/sensor_er.dart'; // Import ERSensor model
@@ -39,163 +40,25 @@ class SensorListViewState extends State<SensorListView> {
     }
   }
 
-  void _addSensor() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        // A Key to validate and save form fields
-        final _formKey = GlobalKey<FormState>();
-
-        String _sensorTag = '';
-        SensorType _selectedSensorType = SensorType.er;
-        ErType _selectedERType = ErType.smallEnd;
-        InnerDiameter _selectedInnerDiameter = InnerDiameter.diameter8;
-        ReferenceSampleArea _selectedReferenceSampleArea =
-            ReferenceSampleArea.area_53_41;
-
-        return AlertDialog(
-          title: const Text('Add New ER Sensor'),
-          content: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'Введите тэг'),
-                    onSaved: (value) {
-                      _sensorTag = value ?? '';
-                    },
-                    // Add validator if needed
-                  ),
-                  DropdownButtonFormField<SensorType>(
-                    decoration: const InputDecoration(labelText: 'Тип датчика'),
-                    value: _selectedSensorType,
-                    items: SensorType.values.map((SensorType type) {
-                      return DropdownMenuItem<SensorType>(
-                        value: type,
-                        child: Text(type.displayString),
-                      );
-                    }).toList(),
-                    onChanged: null, // Make dropdown disabled
-                    onSaved: (SensorType? value) {
-                      if (value != null) {
-                        _selectedSensorType = value;
-                      }
-                    },
-                  ),
-                  DropdownButtonFormField<ErType>(
-                    decoration: const InputDecoration(
-                        labelText: 'Конфигурация чувствительного элемента'),
-                    value: _selectedERType,
-                    items: ErType.values.map((ErType type) {
-                      return DropdownMenuItem<ErType>(
-                        value: type,
-                        child: Text(type.displayString),
-                      );
-                    }).toList(),
-                    onChanged: (ErType? newValue) {
-                      if (newValue != null) {
-                        setState(() {
-                          _selectedERType = newValue;
-                        });
-                      }
-                    },
-                    onSaved: (ErType? value) {
-                      if (value != null) {
-                        _selectedERType = value;
-                      }
-                    },
-                  ),
-                  DropdownButtonFormField<ReferenceSampleArea>(
-                    decoration: const InputDecoration(
-                        labelText: 'Площадь эталонного образца'),
-                    value: _selectedReferenceSampleArea,
-                    items: ReferenceSampleArea.values
-                        .map((ReferenceSampleArea type) {
-                      return DropdownMenuItem<ReferenceSampleArea>(
-                        value: type,
-                        child: Text(type.displayString),
-                      );
-                    }).toList(),
-                    onChanged: (ReferenceSampleArea? newValue) {
-                      if (newValue != null) {
-                        setState(() {
-                          _selectedReferenceSampleArea = newValue;
-                        });
-                      }
-                    },
-                    onSaved: (ReferenceSampleArea? value) {
-                      if (value != null) {
-                        _selectedReferenceSampleArea = value;
-                      }
-                    },
-                  ),
-                  DropdownButtonFormField<InnerDiameter>(
-                    decoration:
-                        const InputDecoration(labelText: 'Внутренний диаметр'),
-                    value: _selectedInnerDiameter,
-                    items: InnerDiameter.values.map((InnerDiameter type) {
-                      return DropdownMenuItem<InnerDiameter>(
-                        value: type,
-                        child: Text(type.displayString),
-                      );
-                    }).toList(),
-                    onChanged: (InnerDiameter? newValue) {
-                      if (newValue != null) {
-                        setState(() {
-                          _selectedInnerDiameter = newValue;
-                        });
-                      }
-                    },
-                    onSaved: (InnerDiameter? value) {
-                      if (value != null) {
-                        _selectedInnerDiameter = value;
-                      }
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
-              },
-            ),
-            TextButton(
-              child: const Text('Confirm'),
-              onPressed: () {
-                // Save form state and create new sensor object
-                if (_formKey.currentState!.validate()) {
-                  _formKey.currentState!.save();
-                  Sensor newSensor = ERSensor(
-                    tag: _sensorTag,
-                    erType: _selectedERType,
-                    referenceSampleArea: _selectedReferenceSampleArea,
-                    innerDiameter: _selectedInnerDiameter,
-                  );
-                  print(newSensor);
-                  // _addSensorToList(newSensor);
-                  Navigator.of(context).pop(); // Close the dialog
-                }
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  void _addSensorToList(Sensor sensor) {
+  // Add sensors
+  void addSensorToList(Sensor sensor) {
     setState(() {
       _localSensors.add(sensor);
     });
   }
 
-  void _removeSensor(Sensor sensor) {
+  void saveSensors() {
+    saveSensorsToFile(_localSensors, SensorType.er);
+  }
+
+  void showAddSensorDialog() {
+    addERSensorDlg(context, addSensorToList, saveSensors);
+  }
+
+  // Remove sensor from the list
+  void showConfirmRemoveDialog() {}
+
+  void removeSensor(Sensor sensor) {
     setState(() {
       _localSensors.remove(sensor);
     });
@@ -218,18 +81,22 @@ class SensorListViewState extends State<SensorListView> {
         ),
         Positioned(
           right: 0,
-          top: 100,
+          top: 36,
           child: FloatingActionButton(
-            onPressed: () => _addSensor(),
+            onPressed: () => showAddSensorDialog(),
             tooltip: 'Add Sensor',
             child: const Icon(Icons.add),
           ),
         ),
-        // FloatingActionButton(
-        //   onPressed: () => _addSensor(),
-        //   tooltip: 'Add Sensor',
-        //   child: const Icon(Icons.add),
-        // ),
+        Positioned(
+          right: 0,
+          top: 100,
+          child: FloatingActionButton(
+            onPressed: () => showConfirmRemoveDialog(),
+            tooltip: 'Remove Sensor',
+            child: const Icon(Icons.remove),
+          ),
+        ),
       ],
     );
   }

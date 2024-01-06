1eebe1de194981776098eab9917654c7c6e929c8
Michael Levin <michael.levi.fx@gmail.com>
2024-01-06 02:45:54 +0200

    add intToHex and parseHex methods

--- flt/plot/lib/src/utils/helpers.dart
+++ flt/plot/lib/src/utils/helpers.dart
@@ -4,3 +4,9 @@ String formatDate(DateTime date) {
   final DateFormat formatter = DateFormat('yyyy-MM-dd');
   return formatter.format(date);
 }
+
+int parseHex(String code) => int.parse(code.replaceFirst('0x', ''), radix: 16);
+
+String intToHex(int value) {
+  return '0x${value.toRadixString(16).toUpperCase()}';
+}

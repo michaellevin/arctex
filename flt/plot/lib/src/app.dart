8baa568d8f30a8bae309977fbcfa70887b4e95bd
Michael Levin <michael.levi.fx@gmail.com>
2024-01-07 03:52:41 +0200

    feat: chart in detached module

--- flt/plot/lib/src/app.dart
+++ flt/plot/lib/src/app.dart
@@ -3,14 +3,13 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:syncfusion_flutter_charts/charts.dart';
+
 import 'services/er_csv_data_parser.dart';
 import 'models/sensors/sensor_abstract.dart';
 import 'models/sensors/sensor_er.dart';
 import 'enums/enums_sensor.dart';
-// import 'utils/helpers.dart';
-
-// import 'ui/sensor_item.dart';
 import 'ui/sensor_view.dart';
+import 'ui/sensor_chart.dart';
 
 class PlotApp extends StatefulWidget {
   const PlotApp({Key? key}) : super(key: key);
@@ -118,32 +117,8 @@ class _PlotAppState extends State<PlotApp> {
             ),
           ),
           isCsvDataLoaded
-              ? SfCartesianChart(
-                  primaryXAxis: const DateTimeAxis(),
-                  primaryYAxis: const NumericAxis(minimum: 0.01, maximum: 0.05),
-                  title: const ChartTitle(text: 'Date/Metal Loss'),
-                  series: <CartesianSeries<dynamic, dynamic>>[
-                    LineSeries<ExtendedCsvData, DateTime>(
-                      dataSource: csvData,
-                      xValueMapper: (ExtendedCsvData data, _) =>
-                          data.originalData.date,
-                      yValueMapper: (ExtendedCsvData data, _) => data.metalLoss,
-                      dataLabelSettings:
-                          const DataLabelSettings(isVisible: false),
-                    )
-                  ],
-                  trackballBehavior: TrackballBehavior(
-                    enable: true,
-                    activationMode:
-                        ActivationMode.singleTap, // or ActivationMode.longPress
-                    tooltipSettings: const InteractiveTooltip(enable: true),
-                  ),
-                  zoomPanBehavior: ZoomPanBehavior(
-                    enablePanning: true,
-                    enablePinching: true,
-                    zoomMode: ZoomMode.x, // Enable zooming along the x-axis
-                    enableMouseWheelZooming: true,
-                  ),
+              ? Expanded(
+                  child: SensorChart(csvData: csvData),
                 )
               : Container()
         ]),
